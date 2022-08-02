0be6453416e6b691ceb6343076b8040e8733e02d
runonamlas <salmannonur@gmail.com>
2022-08-02 18:29:10 +0300

    header color

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 const Layout = ({children, navSelect}) => {
   return <>
     <Head>
-      <title>Taban Kesim Kemer Makineleri Burada - Ayakkabı Makineleri</title>
+      <title>Ayakkabı Taban Kesim Kemer Makineleri Burada - Ayakkabı Makineleri</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content="Sahibinden ayakkabı, taban, kesim, kemer, hazır alt makineleri ve sanayi makineleri alım satım sitesi. İlan vererek kendi makinelerinizi hemen satılık olarak koyabilirsiniz."></meta>
       <link rel="icon" href="/favicon.ico" />
