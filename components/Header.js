b97c86c1a448a2ebce98b2de609944f9fd799a82
runonamlas <salmannonur@gmail.com>
2022-08-02 16:03:23 +0300

    home responsivfe

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ const NavBar = ({navSelect}) => {
       {links.map(link => (
           <li key={link.name} className={ link.select? styles.menuItemSelected : styles.menuItem }>
             <Link key={link.id} href={link.to}><a className={styles.menuText }>
-              <div style={{ display: "flex", alignItems: "center", color: "#2F8F9D"}}>
+              <div className={styles.navDiv}>
                 <Image src={link.icon} alt={link.name} width="32" height="32" /> &nbsp;
                 {link.name}
                 </div>
