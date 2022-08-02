44867b12d00820313dd51e784d1ae3c2dec94db7
runonamlas <salmannonur@gmail.com>
2022-08-02 13:59:56 +0300

    product-detail page redesign

--- pages/ilan/[id].js
+++ pages/ilan/[id].js
@@ -1,16 +1,35 @@
 import axios from "axios";
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/future/image";
 import Link from "next/link";
-import * as React from "react";
+import { useState } from "react";
 import Layout from "../../components/Layout";
 import styles from '../../styles/ProductDetail.module.css';
 
 export default function ProductPage({product, owner, }){
   const images = product.images.split(',')
-  const [image, setImage] = React.useState(images[product.vitrin-1] ? images[product.vitrin-1]: '')
-  const changeImage = (i) => {
-    setImage(i)
+  const [image, setImage] = useState(images[product.vitrin-1] ? images[product.vitrin-1]: '')
+  const [imageIndex, setImageIndex] = useState(images[product.vitrin-1] ? product.vitrin-1 : 0)
+
+  const changeImage = (image, i) => {
+    setImageIndex(i)
+    setImage(image)
+  }
+
+  const leftImage = () => {
+    if(images.length > 1 && imageIndex < images.length && imageIndex > 0){
+      changeImage(images[imageIndex-1], imageIndex-1)
+    }else if(images.length > 1){
+      changeImage(images[images.length-1], images.length-1)
+    }
+  }
+
+  const rightImage = () => {
+    if(images.length > 1 && imageIndex < images.length-1){
+      changeImage(images[imageIndex+1], imageIndex+1)
+    }else if(images.length > 1){
+      changeImage(images[0], 0)
+    }
   }
   const unit = {
     1 : "tl",
@@ -23,7 +42,6 @@ export default function ProductPage({product, owner, }){
     const nameParse = e.charAt(0).toUpperCase()+ e.slice(1) + " "
     nameConfig += nameParse
   })
-
   const used = product.used.charAt(0).toUpperCase()+ product.used.slice(1)
 
   return (<Layout>
@@ -32,65 +50,61 @@ export default function ProductPage({product, owner, }){
     </Head>
     <main className={styles.main}>
       <h1 className={styles.productTitle}>{product.name}</h1>
-      <section className={styles.sliderSection}>
-        
-      </section>
-      <section className={styles.contentSection}>
-
-      </section>
-     {/*
-       <div className={styles.left}>
-        <div className={styles.imageTop}>
-          <button type="button" name="sag" onClick={()=>{}} className={styles.leftbutton}>&lt;</button>
-          <Image className={styles.productimage} src={image}  height='700' width='700'/>
-          <button type="button" name="sol" onClick={()=>{}} className={styles.rightbutton}>&gt;</button>
-
-        </div>
-        <div className={styles.imageDown}>
-        { images.map(imageUrl => {
-            return <Image key={imageUrl} className={styles.productimage} src={imageUrl} onClick={()=>{changeImage(imageUrl)}}  height='100' width='100'/>
+      <section className={styles.productSection}>
+        <div className={styles.sliderSection}>
+          <div className={styles.smallPhotoArea}>
+            { 
+            images.map((imageUrl, i) => {
+              return <div key={imageUrl}  className={styles.smallPhotoOne}>
+              <Image key={imageUrl} className={styles.productSmallImage} alt="product-image" src={imageUrl} onClick={()=>{changeImage(imageUrl,i)}} height='100' width='300'/>
+              </div>
+            })
             }
-          )}
           </div>
-      </div>
-      <div className={styles.right}>
-        <h1 className={styles.productTitle}>{product.name}</h1>
-        <div className={styles.productDetailDiv}>
-          <a className={styles.productDetailTitle}>Satıcı</a>
-          <Link href={`/magaza/${product.users.username}`}><a className={styles.productOwner}>{nameConfig.replace(/\s+$/g, '')}</a></Link>
+          <div className={styles.photoArea}>
+            <button type="button" name="sag" onClick={()=>{leftImage()}} className={styles.leftbutton}>&lt;</button>
+            <button type="button" name="sol" onClick={()=>{rightImage()}} className={styles.rightbutton}>&gt;</button>
+            <div className={styles.photoDiv}>
+              <Image priority="true" className={styles.productImage} src={image}  height='700' width='700'/>
+            </div>
+          </div>
         </div>
-        <div className={styles.productDetailDiv}>
-          <a className={styles.productDetailTitle}>İlan Tarihi</a>
-          <a className={styles.productDetailData}>{product.createdAt.substring(0,10)}</a>
+        <div className={styles.contentSection}>
+          <div className={styles.productDetailDiv}>
+            <a className={styles.productDetailTitle}>Satıcı</a>
+            <Link href={`/magaza/${product.users.username}`}><a className={styles.productOwner}>{nameConfig.replace(/\s+$/g, '')}</a></Link>
+          </div>
+          <div className={styles.productDetailDiv}>
+            <a className={styles.productDetailTitle}>İlan Tarihi</a>
+            <a className={styles.productDetailData}>{product.createdAt.substring(0,10)}</a>
+          </div>
+          <div className={styles.productDetailDiv}>
+            <a className={styles.productDetailTitle}>İlan Adresi</a>
+            <a className={styles.productDetailData}>{product.users.address.split('%')[1]}</a>
+          </div>
+          <div className={styles.productDetailDiv}>
+            <a className={styles.productDetailTitle}>Marka</a>
+            <a className={styles.productDetailData}>{product.brand}</a>
+          </div>
+          <div className={styles.productDetailDiv}>
+            <a className={styles.productDetailTitle}>Durumu</a>
+            <a className={styles.productDetailData}>{used}</a>
+          </div>
+          <div className={styles.priceAndContactDiv}>
+             <a className={styles.productPrice}>{product.price} {unit[product.priceUnit]}</a>
+            {!owner && <>
+              <Link href={{pathname: `/mesaj/${product.users.id}-${product.users.username}`, query: { product: product.id, productName: product.name} }}>
+                <div className={styles.mesajButton}>mesaj gönder</div>
+              </Link>
+              <Link href={`tel:${product.users.callNumber}`} ><div className={styles.callButton}>
+                  ara ({product.users.callNumber})</div>
+              </Link>
+            </>
+            }
+          </div>
         </div>
-        <div className={styles.productDetailDiv}>
-          <a className={styles.productDetailTitle}>İlan Adresi</a>
-          <a className={styles.productDetailData}>{product.users.address.split('%')[1]}</a>
-        </div>
-        <div className={styles.productDetailDiv}>
-          <a className={styles.productDetailTitle}>Marka</a>
-          <a className={styles.productDetailData}>{product.brand}</a>
-        </div>
-        <div className={styles.productDetailDiv}>
-          <a className={styles.productDetailTitle}>Durumu</a>
-          <a className={styles.productDetailData}>{used}</a>
-        </div>
-        <a className={styles.productPrice}>{product.price} {unit[product.priceUnit]}</a>
-        {!owner && <>
-          <Link href={{pathname: `/mesaj/${product.users.id}-${product.users.username}`, query: { product: product.id, productName: product.name} }}><div className={styles.mesajButton}>
-          mesaj gönder</div>
-        </Link>
-        <Link href={`tel:${product.users.callNumber}`} ><div className={styles.callButton}>
-            ara ({product.users.callNumber})</div>
-        </Link>
-        </>
-        }
-        
-
-      </div>
-      */  }
+      </section>
     </main>
-    
   </Layout>)
 }
 
