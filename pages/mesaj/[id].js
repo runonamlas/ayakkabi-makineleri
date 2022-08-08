abfb356b58e56016cdf18af3ca2e380f368bb144
runonamlas <salmannonur@gmail.com>
2022-08-08 12:35:43 +0300

    messages config

--- pages/mesaj/[id].js
+++ pages/mesaj/[id].js
@@ -1,20 +1,16 @@
 import axios from "axios";
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/future/image";
 import Link from "next/link";
 import { parseCookies } from "nookies";
-import * as React from "react";
+import { useEffect, useState } from "react";
 import styles from '../../styles/MessagePage.module.css';
 
-function MessagePage({data, messages, goster, product}){
-  const [buttonState, setButtonState] = React.useState(false);
-  const [image, setImage] = React.useState([]);
-  const [message, setMessage] = React.useState('')
-  React.useEffect(() => {
-    if(product.id){
-      setImage(product.images.split(","))
-    }
-  }, []);
+function MessagePage({data, messages, goster, product}){ 
+  const [magazaName, setMagazaName] = useState(data ? data[1] : '');
+  const [image, setImage] =useState(product.id ? product.images.split(",") : []);
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState(messages);
   const unit = {
     1: "tl",
     2: "£",
@@ -22,7 +18,7 @@ function MessagePage({data, messages, goster, product}){
   }
 
   const submitMessage = async (event) => {
-    setButtonState(true);
+    event.preventDefault();
     const cookies = parseCookies()
     const userID = parseInt(data[0])
     var groupData = {}
@@ -43,43 +39,41 @@ function MessagePage({data, messages, goster, product}){
     }
     axios.defaults.headers.common['Authorization'] = cookies.OursiteJWT;
     await axios.post(process.env.NEXT_PUBLIC_AXIOS_CONF+"/messages/",groupData).then(response => {
-      setButtonState(false);
+      setMessageList(current => [...current, response.data.data]);
+      setMessage('');
     }).catch((error) => {
       console.error('Error:', error.response);
     });
   }
-
   return (
     <main className={styles.main}>
        <Head>
       <title>{data[1]}</title>
     </Head>
-      <Link href={`/magaza/${data[0]}-${data[1]}`}>
-        <a>
-          <div className={styles.OwnerTitle}>
-        <a>{data[1]}</a>
-        <Image alt="gönder" className={styles.productimage} src='/icons/right-arrow-white.svg' height='20' width='20' />
-      </div>
-        </a>
+      <Link rel="prefetch" href={`/magaza/${data[0]}-${data[1]}`}>
+          <a className={styles.OwnerTitle}>
+      {magazaName}
+        <Image alt="gönder" key="left-button" className={styles.productimage} src='/icons/right-arrow-white.svg' height='20' width='20' />
+      </a>
       </Link>
       <div id="chatView" className={styles.chatView} ref={node => {if(node) node.scrollTop = node.scrollHeight }}>
-      {messages.map(e=>{
-        const imageArray = e.product.images.split(",")
-        if(e.owner.id == data[0]){
-          return <div className={styles.chatSatirLeft}>
+      {Object.keys(messageList).map((e)=>{
+        const imageArray = messageList[e].product.images.split(",")
+        if(messageList[e].owner.id == data[0]){
+          return <div key={e} className={styles.chatSatirLeft}>
           <div className={styles.chatDiv}>
             <div className={styles.productCardM}>
-              <Image priority="true" alt="ayakkabı makinesi fotografı" key={e.id} className={styles.productimageM} src={imageArray[e.product.vitrin - 1]} height='100' width='140'/>
-              <a className={styles.productTitleM}>{e.product.name}</a>
-              <a className={styles.productPriceM}>{e.product.price} {unit[e.product.priceUnit]}</a>
+              <Image priority="true" alt="ayakkabı makinesi fotografı" key={messageList[e].id} className={styles.productimageM} src={imageArray[messageList[e].product.vitrin - 1]} height='100' width='140'/>
+              <a className={styles.productTitleM}>{messageList[e].product.name}</a>
+              <a className={styles.productPriceM}>{messageList[e].product.price} {unit[messageList[e].product.priceUnit]}</a>
             </div>
-            <a className={styles.chatText}>{e.name}</a>
+            <a className={styles.chatText}>{messageList[e].name}</a>
           </div>
         </div>
         }else {
-          return <div className={styles.chatSatirRight}>
+          return <div key={e} className={styles.chatSatirRight}>
             <div className={styles.chatDivR}>
-              <a className={styles.chatText}>{e.name}</a>
+              <a className={styles.chatText}>{messageList[e].name}</a>
             </div>
           </div>
         }
@@ -87,21 +81,22 @@ function MessagePage({data, messages, goster, product}){
       </div>
       <div className={styles.chatWrite}>
         {goster && <div className={styles.productCard}>
-              <Image priority="true" alt="ayakkabı makinesi fotografı"  className={styles.productimage} src={image[product.vitrin - 1]} height='100' width='140'/>
+              <Image src={image[product.vitrin-1]} key="mesaj-ilan" priority="true" alt='ayakkabi-makinesi-fotografi'  className={styles.productimage} height='100' width='140'/>
               <a className={styles.productTitle}>{product.name}</a>
               <a className={styles.productPrice}>{product.price} {unit[product.priceUnit]}</a>
-            </div>}
+    </div>}
         <div>
           <form onSubmit={submitMessage}>
             <input required type="text" className={styles.chatInput} id="mesaj" name="mesaj" placeholder="bla bla bla" value={message} onChange={(e) => setMessage(e.target.value)}   />
-            <input type="image" src="/icons/right-arrow.svg" className={styles.writeButton} alt="Submit" width="48" height="48" disabled={buttonState}/>
+            <input type="image" src="/icons/right-arrow.svg" className={styles.writeButton} alt="Submit" width="48" height="48"/>
           </form>
           
         </div>
       
       </div>
      
-    </main>)
+    </main>
+    )
 }
 
 export const getServerSideProps = async (context) => {
