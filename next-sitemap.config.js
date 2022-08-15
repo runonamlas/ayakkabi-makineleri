b4176a5ae30d3ba229f947a1b09f577aa7a095c4
runonamlas <salmannonur@gmail.com>
2022-08-15 14:13:10 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://ayakkabimakineleri.com' || 'https://www.ayakkabimakineleri.com',
+  generateRobotsTxt: true,
+}
+
+export default config
