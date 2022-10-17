6553ba4bda182401c504e4443d99a8a51373509a
runonamlas <salmannonur@gmail.com>
2022-10-17 10:04:03 +0300

    lolom

---
+++ pages/privacy.js
@@ -0,0 +1,103 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/NotFound.module.css'
+
+export default function Notfound() {
+  return (
+      <section className={styles.section}>
+        <Head>
+          <title>gizlilik politikası- ayakkabi makineleri</title>
+          <meta name='description' content='gizlilik politikası' />
+          <meta name='twitter:url' content='https://ayakkabimakineleri.com/privacy' />
+          <meta name='twitter:title' content='gizlilik politikası- ayakkabi makineleri' />
+          <meta name='twitter:description' content='gizlilik politikası' />
+          <meta property='og:title' content='gizlilik politikası- ayakkabi makineleri' />
+          <meta property='og:description' content='gizlilik politikası' />
+          <meta property='og:url' content='https://ayakkabimakineleri.com/privacy' />
+        </Head>
+        <main className={styles.main}>
+          <h1 className={styles.hone}>gizlilk politikası</h1><br/>
+          <h3 className={styles.hthere}>
+          1. Giriş
+
+Web sitesi ziyaretçilerimizin ve hizmet kullanıcılarımızın gizliliğini korumayı taahhüt ediyoruz.
+Bu politika, bu tür kişilerin kişisel verileriyle ilgili olarak bir veri denetleyicisi olarak hareket ettiğimiz durumlarda geçerlidir; başka bir deyişle, bu kişisel verilerin işlenme amaçlarını ve araçlarını belirlediğimiz durumlarda.
+Web sitemiz, kişisel verilerinizi nasıl işleyeceğimizi etkileyen gizlilik kontrolleri içermektedir. Gizlilik kontrollerini kullanarak kişisel verilerinizin toplanmasını, paylaşılmasını ve yayınlanmasını sınırlayabilirsiniz.
+Web sitemizde tanımlama bilgileri kullanıyoruz. Web sitemizi ilk ziyaret ettiğinizde sizden çerez kullanımımıza izin vermenizi isteyeceğiz.
+2. Topladığımız kişisel veriler
+
+Sizinle iletişim kurmamızı sağlayan verileri işleyebiliriz (adınız, e-posta adresiniz, telefon numaranız, posta adresiniz ve / veya sosyal medya hesap tanımlayıcılarınız gibi)
+Web sitenizin kullanıcı hesabı verilerini işleyebiliriz (hesap tanımlayıcınızı, adınızı, e-posta adresinizi, işletme adınızı, hesap oluşturma ve değiştirme tarihlerini, web sitesi ayarlarını ve pazarlama tercihlerini içerebilir).
+Web sitemiz aracılığıyla girdiğiniz mal ve / veya hizmet satın alımları dahil işlemlerle ilgili bilgileri işleyebiliriz (adınız, iletişim bilgileriniz, ödeme kartı bilgileriniz veya diğer ödeme ayrıntılarınız ve işlem ayrıntıları).
+Web sitemizi ve hizmetlerimizi kullanımınızla ilgili verileri işleyebiliriz.
+Kullanım verileri şunları içerebilir:
+IP adresiniz, coğrafi konumunuz, tarayıcı türünüz ve sürümünüz, işletim sisteminiz,
+yönlendirme kaynağınız, ziyaret süreniz, sayfa görünümleriniz ve web sitesi gezinme yollarınızın yanı sıra, hizmet kullanımınızın zamanlaması, sıklığı ve kalıbı hakkındaki bilgiler,
+e-postalarımız ve/veya bültenlerimize abone olurken verdiğiniz ad ve e-posta adresi gibi bilgiler, kullanıcı adınız, profil resminiz ve gönderinizin içeriği dahil, sitemizi kullanarak internette paylaşmak amacıyla sitemize gönderdiğiniz bilgiler,
+iletişim içerikleri ve üst veriler dahil sitemiz veya e-posta yoluyla gönderdiğiniz her türlü iletişim içerikleri, bize gönderdiğiniz diğer tüm kişisel bilgiler.
+3. Kişisel bilgilerinizin kullanılması
+
+Kişisel bilgilerinizi şu amaçlar için kullanabiliriz: sitemizi ve işletmemizi yönetmek; sizin için kişiselleştirerek web sitemizdeki hizmetleri kolaylıkla kullanmanızı sağlamak
+Satın alma işlemlerinde ürünleri size kolaylıkla ulaştırabilmek; bildirim, fatura ve ödeme hatırlatıcıları göndermek ve sizden ödeme almak,
+Talep ettiğiniz e-posta bildirimlerini göndermek (bildirimleri dilediğiniz zaman iptal edebilirsiniz)
+Size işletmemiz veya özenle seçilmiş üçüncü taraf işletmeler tarafından, işinize yarayacağını düşündüğümüz pazarlama içeriklerini özellikle kabul ettiğiniz takdirde posta, e-posta veya benzer teknolojilerle göndermek (pazarlama içeriklerini istemiyorsanız bize her an bildirebilirsiniz)
+Sitemizle ilgili sizin tarafınızdan veya sizinle ilgili yapılan şikayetleri ve talepleri çözmek
+Sitemizi güvenli tutmak ve dolandırıcılığı önlemek
+Üçüncü taraflara kullanıcılarımız hakkında istatistiksel bilgiler sunmak (ancak bu üçüncü taraflar bu bilgileri kullanarak hiçbir kullanıcının kimliğini belirleyemezler)
+Web sitemizin, ilgili şartlar ve koşullara uygun olarak kullanıldığını doğrulamak (sitemizdeki özel mesaj hizmeti ile gönderilen mesajları izlemek dahil).
+Kişisel verilerinizi, tabi olduğumuz yasal bir yükümlülüğe uymak için veya hayati menfaatlerinizi veya başka bir gerçek kişinin hayati menfaatlerini korumak için gerekli olduğu durumlarda da kullanabiliriz.
+4. Kişisel verilerin paylaşılması
+
+Kişisel verilerinizi sigortacılarımıza ve / veya profesyonel danışmanlarımıza, sigorta kapsamının alınması veya sürdürülmesi, risklerin yönetilmesi, profesyonel tavsiye alma amaçları için makul ölçüde gerekli olduğu sürece ifşa edebiliriz.
+Kişisel verilerinizi yasal gereklilik durumlarında ilgili mercilerle paylaşabiliriz (mahkeme işlemlerinde veya idari veya mahkeme dışı bir prosedürde yasal iddiaların oluşturulması, uygulanması veya savunulması için bu tür bir ifşanın gerekli olduğu durumlarda kişisel verilerinizi paylaşabiliriz.
+Bu ilkeler dışında, açıkça izin vermediğiniz müddetçe kişisel bilgilerinizi üçüncü taraflara iletmeyeceğiz.
+
+5. Kişisel verilerin saklanması ve silinmesi
+
+Herhangi bir amaçla işlediğimiz kişisel veriler, bu amaçlar için gerekli olandan daha uzun süre saklanmayacaktır. Bu ilkeyi etkilememek üzere, aşağıda belirtilen kategorilere dahil olan verileri genellikle aşağıda belirtilen tarih/saatte sileriz:
+Kişisel veri türü oluşturulduktan 5 yıl sonra silinecektir.
+Diğer hükümlerine bakılmaksızın, tabi olduğumuz yasal bir yükümlülüğe uymak için veya hayati çıkarlarınızı veya hayati çıkarlarınızı korumak için, bu tür bir saklamanın gerekli olduğu durumlarda kişisel verilerinizi saklayabiliriz.
+6. Haklarınız
+
+Veri koruma yasası kapsamındaki temel haklarınız şunlardır:
+
+Erişim hakkı – kişisel verilerinizin kopyalarını isteyebilirsiniz;
+Düzeltme hakkı – bizden yanlış kişisel verileri düzeltmemizi ve eksik kişisel verileri tamamlamamızı isteyebilirsiniz;
+Silme hakkı – bizden kişisel verilerinizi silmemizi isteyebilirsiniz;
+İşlemeyi kısıtlama hakkı – bizden kişisel verilerinizin işlenmesini kısıtlamamızı isteyebilirsiniz;
+İşlemeye itiraz etme hakkı – kişisel verilerinizin işlenmesine itiraz edebilirsiniz;
+Veri taşınabilirliği hakkı – kişisel verilerinizi başka bir kuruluşa veya size aktarmamızı isteyebilirsiniz;
+Bir denetim makamına şikayette bulunma hakkı – kişisel verilerinizi işlememiz hakkında şikayette bulunabilirsiniz;
+Rızanızı geri çekme hakkı – kişisel verilerinizi işlememizin yasal dayanağının rıza olduğu ölçüde, bu rızayı geri çekebilirsiniz.
+Kişisel verilerinizle ilgili haklarınızdan herhangi birini [aşağıda belirtilen iletişim bilgilerini kullanarak bize yazılı bildirimde bulunarak] kullanabilirsiniz.
+
+7. Çerezler
+
+Web sitemizde çerezler kullanılır. Bu çerezler, ziyaretçilerin tercihlerini ve ziyaretçinin eriştiği veya ziyaret ettiği web sitesindeki sayfaları içeren bilgileri depolamak için kullanılır. Bilgiler, web sayfası içeriğimizi ziyaretçilerin tarayıcı türüne ve / veya diğer bilgilere göre özelleştirerek kullanıcıların deneyimini optimize etmek için kullanılır.
+
+Çerezler “kalıcı” veya “oturum” çerezleri olabilir: Kalıcı çerez, kullanıcı tarafından son kullanım tarihine kadar silinmediği takdirde tarayıcı tarafından saklanır ve son kullanım tarihinde silinir; oturum çerezi ise kullanıcı oturumunun sonunda tarayıcı kapatıldığında silinir. Çerezler, bir kullanıcıyı kişisel olarak tanımlayan herhangi bir bilgi içeremez, ancak hakkınızda sakladığımız kişisel veriler, çerezlerde saklanan ve bunlardan elde edilen bilgilerle bağlantılı olabilir.
+
+Çerezler hakkında daha genel bilgi için, lütfen Çerez Onayı web sitesindeki ” Çerez Nedir” makalesini okuyun.
+
+Sitemizdeki bazı reklamcılar çerezler ve web işaretçileri kullanabilir. Reklam ortaklarımızın her birinin, kullanıcı verilerine ilişkin politikaları için kendi Gizlilik Politikası vardır. Web sitemizin üçüncü taraf reklamcılar tarafından kullanılan tanımlama bilgilerine erişimi veya kontrolü olmadığını unutmayın. Bu nedenle, daha ayrıntılı bilgi için bu üçüncü taraf reklam sunucularının ilgili Gizlilik Politikalarına başvurmanızı tavsiye ederiz.
+
+8. Çerezleri yönetme
+
+Çoğu tarayıcı, çerezleri kabul etmenize, reddetmenize ve silmenize izin verir. Şu bağlantılar aracılığıyla çerezleri engelleme ve silme hakkında güncel bilgiler edinebilirsiniz:
+
+https://support.google.com/chrome/answer/95647 (Chrome)
+https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences (Firefox);
+https://help.opera.com/en/latest/security-and-privacy/ (Opera);
+https://support.microsoft.com/en-gb/help/17442/windows-internet-explorer-delete-manage-cookies (Internet Explorer);
+https://support.apple.com/en-gb/guide/safari/manage-cookies-and-website-data-sfri11471/mac (Safari); ve
+https://privacy.microsoft.com/en-us/windows-10-microsoft-edge-and-privacy (Edge).
+Tüm tanımlama bilgilerinin engellenmesi, birçok web sitesinin kullanılabilirliği üzerinde olumsuz bir etkiye sahip olacaktır. Çerezleri engellerseniz, web sitemizdeki tüm özellikleri kullanamazsınız.
+
+Web sitemizde yeni bir versiyon yayınlayarak bu politikayı zaman zaman güncelleyebiliriz. Bu Gizlilik Politikası yalnızca çevrimiçi etkinliklerimiz için ve web sitemizi ziyaret edenler için geçerlidir. Bu politika, çevrimdışı veya bu web sitesi dışındaki kanallar aracılığıyla toplanan hiçbir bilgi için geçerli değildir.
+
+Web sitemizi kullanarak, Gizlilik Politikamızı, Şartlar ve Koşulları kabul etmiş sayılırsınız.
+          </h3>
+        </main>
+      </section>
+  )
+}
